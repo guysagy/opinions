666baee1052a2f6079c13d68cf23be7e909ce7d2
Guy Sagy <guysagy@hotmail.com>
2025-05-08 14:27:32 -0700

    Layout Styling

---
+++ src/app/reviews/stardew-valley/page.jsx
@@ -0,0 +1,10 @@
+export default function StradeValleyPage() {
+    return (
+        <>
+            <h1>Stardew Valley</h1>
+            <p>
+                This will be the review for Stardview Valley
+            </p>
+        </>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/app/**/*.{js,ts,jsx,tsx}', // Adjust paths as needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
