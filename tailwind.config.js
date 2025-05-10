be5a78fb6f6fc5d0a62dbb1823410a22ad2da5c0
Guy Sagy <guysagy@hotmail.com>
2025-05-09 21:01:24 -0700

    custom css fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     content: [
-      './src/app/**/*.{js,ts,jsx,tsx}', // Adjust paths as needed
+        './src/app/**/*.{js,ts,jsx,tsx}', // Adjust paths as needed
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/app/fonts.js',
     ],
     theme: {
-      extend: {},
+        extend: {
+            fontFamily: {
+                orbitron: ['var(--font-orbitron)', 'sans']
+            }
+        },
     },
     plugins: [],
-  };
-  
+};
