a32fc001c57f692bfc3438eb53854d598d82922a
Guy Sagy <guysagy@hotmail.com>
2025-05-10 10:04:05 -0700

    responsive design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Heading from '@/components/Heading';
 
 export default function HomePage() {
@@ -5,9 +6,18 @@ export default function HomePage() {
     return (
         <>
             <Heading>Indie Gamer</Heading>
-            <p>
+            <p className="pb-3">
                 Only the best indie games, reviewed for you.
             </p>
+            <div className="bg-white border border-gray-500 rounded shadow w-80 hover:shadow-xl sm:w-full">
+                <Link className="flex flex-col sm:flex-row" href="/reviews/stardew-valley">
+                    <img src="/images/stardew-valley.jpg" alt="" 
+                        width="320" height="180" className="rounded-t sm:rounded-l sm:rounded-r-none" />
+                    <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+                        Stardew Valley
+                    </h2>
+                </Link>
+            </div>
         </>
     );
 }
