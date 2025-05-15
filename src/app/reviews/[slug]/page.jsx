c48cafcb5caf99d97e9fbe71941717a80b15a085
Guy Sagy <guysagy@hotmail.com>
2025-05-14 23:01:59 -0700

    heroicons

--- src/app/reviews/[slug]/page.jsx
+++ src/app/reviews/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import { getReview, getSlugs } from '@/lib/reviews';
 import Heading from '@/components/Heading';
+import ShareLinkButton from '@/components/ShareLinkButton';
 
 export async function generateStaticParams() {
     const slugs = await getSlugs();
@@ -23,7 +24,10 @@ export default async function ReviewPage(props) {
     return (
         <>
             <Heading>{review.title}</Heading>
-            <p className="italic pb-2">{review.date}</p>
+            <div className="flex gap-3 items-baseline">
+                <p className="italic pb-2">{review.date}</p>
+                <ShareLinkButton />
+            </div>
             <img src={review.image} alt="" 
                 width="640" height="360" className="mb-2 rounded">
             </img>
