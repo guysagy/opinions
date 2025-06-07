2a3fe5fbdf0c78a29ad42d365081f4d715a5bfbf
Guy Sagy <guysagy@hotmail.com>
2025-06-06 23:33:10 -0700

    usePathname

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function NavLink({children, href, prefetch, className}) {
+    className = className || '';
+    const pathname = usePathname();
+    console.log('NavLink pathname = ', pathname);
+    if (pathname === href) {
+        return (
+            <span className={`${className} text-orange-800`}>{children}</span>
+        );
+    }
+    return (
+        <Link href={href} prefetch={prefetch} className={`${className} text-orange-800 hover:underline`}>{children}</Link>
+    );
+};
--- src/components/PaginationBar.jsx
+++ src/components/PaginationBar.jsx
@@ -4,18 +4,30 @@ import { ChevronLeftIcon , ChevronRightIcon } from '@heroicons/react/20/solid';
 const PaginationBar = ({url, page, pageCount}) => {
     return (
         <div className="flex gap-2 pb-3">
-            <PaginationLink href={`${url}?page=${page-1}`}>
+            <PaginationLink href={`${url}?page=${page-1}`} enabled={page>1}>
                 <ChevronLeftIcon className="h-5 w-5"/>
+                <span className="sr-only">Previous page</span>
             </PaginationLink>
             <span>Page {page} of { pageCount }</span>
-            <PaginationLink href={`${url}?page=${page+1}`}>
+            <PaginationLink href={`${url}?page=${page+1}`} enabled={page<pageCount}>
                 <ChevronRightIcon className="h-5 w-5"/>
+                <span className="sr-only">Next page</span>
             </PaginationLink>
         </div>
     );
 }
 
-function PaginationLink({href, children}) {
+function PaginationLink({href, children, enabled}) {
+    if (!enabled) {
+        return (
+            <span
+                className="border cursor-not-allowed rounded text-slate-500 text-sm"
+            >
+                {children}
+            </span>         
+        );
+    }
+
     return (
         <Link href={href}
             className="border rounded text-slate-500 text-sm
