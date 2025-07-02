ca3604a57a7c384d854242655009412b2906217c
Guy Sagy <guysagy@hotmail.com>
2025-07-01 20:14:38 -0700

    Prevent duplicate comments submissions

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -7,21 +7,22 @@ import { createCommentAction } from '@/app/reviews/[slug]/actions';
 
 export default function CommentForm({slug, title}) {
     const router = useRouter();
-    const [error, setError] = useState(null);
+    const [state, setState] = useState({ loading: false, error: null });
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setError(null);
+        setState({loading: true, error: null});
         const form = event.currentTarget;
         const formData = new FormData(form);
         console.log('formData = ', formData);
         try {
             const result = await createCommentAction(formData);
             if (result?.isError) {
-                setError(result);
+                setState({loading: false, error: result.message});
             } else {
                 form.reset();
                 router.push(`/reviews/${slug}`, { scroll: false });
+                setState({loading: false, error: null});
             }
         } catch (error) {
             console.log('[CommentForm] error : ', error);
@@ -52,13 +53,14 @@ export default function CommentForm({slug, title}) {
                 />
             </div>
             {
-                Boolean(error) && (
-                    <p className="text-red-700">{error.message}</p>
+                Boolean(state.error) && (
+                    <p className="text-red-700">{state.error}</p>
                 )
             }
-            <button type='submit'
+            <button type='submit' disabled={state.loading}
                 className="bg-orange-800 rounded px-2 py-1 self-center
-                            text-slate-50 w-32 hover:bg-orange-700">
+                            text-slate-50 w-32 hover:bg-orange-700
+                            disabled:bg-slate-500 disabled:cursor-not-allowed">
                 Submit
             </button>
         </form>
